55e6076e9be3b9734a71f3c4bb2c2eaf36dfbde5
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2020-10-11 23:46:20 -0600

    break commit

--- main.c
+++ main.c
@@ -14,11 +14,23 @@
 
 #include <pic16f688.h>
 
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/OSC2/CLKOUT pin, I/O function on RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select bit (MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Detect (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+
 //PORTA defines
 //Inputs
 #define DCAC        RA0
 #define VHZ         RA1
 #define BKOUT       RA2
+//skipped MCLR
 
 //Outputs
 #define DCAC_HOOK   RA4
@@ -34,17 +46,24 @@
 
 void main(void)
 {
-    ANSEL = 0x00;
-    CMCON0 = 0x07;
+    ANSEL = 0x00; //AN pins set as digital i/o
+    CMCON0 = 0x07;//comparators are of, CxIN pins as digital i/o
     
     TRISA = 0x07; //RA0,1,2 nputs. others outputs
     PORTA = 0xF8; //PORTA outputs set to mcu logic high
+    OPTION_REGbits.nRAPU = 0; //Enable PORTA individual internal weak pull up resistors
+    WPUA  = 0x07; //RA0,1,2 enable weak internal pull ups
     
     TRISC = 0x00; //all outputs
     PORTC = 0xFF; // PORTC outputs set to mcu logic high
     
-    //check VHZ input on reset
-    __delay_ms(50);
+    //MCU logic level 1(high) is DMM logic level 0(low) and vice versa
+    
+    
+    
+    //check VHZ input on reset to set
+    __delay_ms(100);
+    
     
 }
 
